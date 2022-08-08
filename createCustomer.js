5191fa057245c5a727d19f0f3c325e3605b77980
carlos <carlos.rodrigues@grupoboticario.com.br>
2022-08-08 15:09:19 -0300

    Ajustes para retry

--- createCustomer.js
+++ createCustomer.js
@@ -2,13 +2,14 @@
 const AWS = require('aws-sdk');
 
 module.exports.createCustomer = async (event) => {
-  const body = JSON.parse(Buffer.from(event.body, 'base64').toString());
+  // const body = JSON.parse(Buffer.from(event.body, 'base64').toString());
+  const body = JSON.parse(event.body);
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
   const putParams = {
     TableName: process.env.DYNAMODB_TABLE,
     Item: {
-      primary_key: body.key,
-      text: body.value,
+      primary_key: body.key.toUpperCase(),
+      text: body.key.toLowerCase(),
     },
   };
   await dynamoDb.put(putParams).promise();
--- handler.js
+++ handler.js
@@ -17,24 +17,35 @@ module.exports.functionA = async (event) => {
 
 module.exports.functionB = async (event) => {
   const body = JSON.parse(event.body);
-
+  console.log(body);
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
   const params = {
     TableName : process.env.DYNAMODB_TABLE,
     Key: {
-      "primary_key": {S: body.key}
+      "primary_key": body.value.key
     }
   };
 
   const reg = await dynamoDb.get(params).promise();
+  console.log(reg.Item);
+
+  if (!reg["Item"]) {
+    function MustRetryError(message) {
+        this.name = 'MustRetryError';
+        this.message = message;
+    }
+    MustRetryError.prototype = new Error();
+
+    throw new MustRetryError('This is a custom error!');
+  }
 
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         input: event,
-        value: !!reg
+        value: reg["Item"]
       },
       null,
       2
@@ -44,13 +55,34 @@ module.exports.functionB = async (event) => {
 
 module.exports.functionC = async (event) => {
   const body = JSON.parse(event.body);
+  console.log(body.value);
+
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+  const params = {
+    TableName : process.env.DYNAMODB_TABLE,
+    Key: {
+      "primary_key": body.value.primary_key
+    },
+    UpdateExpression: "set #text = :newText",
+    ExpressionAttributeValues: {
+      ":newText": body.value.primary_key.toLowerCase()+":functionC",
+    },
+    ExpressionAttributeNames: {
+      "#text": "text",
+    },
+    ReturnValues: "UPDATED_NEW",
+  }
+
+
+  const reg = await dynamoDb.update(params).promise()
 
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         input: event,
-        value: {...body, output: "BBB"}
+        value: reg.Item
       },
       null,
       2
